9329040d44aad376aa33594f62312c5ed07afe21
sunilgouli <sunilgouli14@gmail.com>
2022-04-10 12:36:15 +0530

    Sample change

--- Workspace/src/practice/ForDemo.java
+++ Workspace/src/practice/ForDemo.java
@@ -3,11 +3,11 @@ package practice;
 public class ForDemo {
 
 	public static void main(String[] args) {
-     for (int i=0; i<=20; i=i+2)
-     {
-    	 System.out.println(i);
-     }
-
+		for (int i = 0; i <= 20; i = i + 2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Hiiiiiiiiiiiiiiiiii");
 	}
 
 }
